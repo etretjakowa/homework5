dbe352ee30a571d588c7c4e11c5ec592ee03c680
Елена <etretjakowa@yandex.ru>
2021-11-04 17:14:18 +0700

    ДЗ"Массивы"

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,46 @@
 package ru.skypro;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //int[] arr = generateRandomArray();
+
+        // public static int[] generateRandomArray() {
+        //   java.util.Random random = new java.util.Random();
+        Random random = new Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 200_000;
+        }
+        System.out.println("arr:" + Arrays.toString(arr));
+        int sum = 0;
+        for (int value : arr) {
+            sum += value;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int value = arr[i];
+            if (value < min) {
+                min = value;
+            } else if (value > max) {
+                max = value;
+            }
+        }
+        System.out.println("Минимальная сумма трат за месяц составила " + min + " рублей. Максимальная сумма трат за месяц" +
+                " составила " + max + " рублей.");
+        System.out.println("Средняя сумма трат за месяц составила " + sum / (float) arr.length + " рублей");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
     }
+
 }
